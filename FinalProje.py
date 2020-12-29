7e6ef0483a3d48ba1849d1783b652187ebdf5eb0
mehmetarsay <mehmetarsay5@gmail.com>
2020-12-29 23:33:44 +0300

    -Mouse control düzenlendi -Zıplama eklendi

--- FinalProje.py
+++ FinalProje.py
@@ -6,6 +6,8 @@ from Dog import *
 import math as m
 from MapTexture import*
 
+windowX = 1920
+windowY=1080
 class Camera():
     angleX = 0.0
     angleY = 0.05
@@ -22,7 +24,9 @@ class Camera():
     mouse_x =0
     mouse_y = 0
     mouse_left=1
-
+    humanSpace = 0
+    x =0
+    y= 0
 camera =Camera()
 
 def getHuman():
@@ -30,7 +34,8 @@ def getHuman():
     glPushMatrix()
     glTranslatef(0, 5, 0)
     #glTranslatef(camera.xPos+8*camera.directionX, 0, (camera.zPos)+8*camera.directionZ)
-    glTranslatef(camera.xPos+8*camera.directionX , 0, (camera.zPos)+8*camera.directionZ )
+    glTranslatef(camera.xPos+8*camera.directionX , camera.humanSpace, (camera.zPos)+8*camera.directionZ )
+    HumanSpace()
     glRotatef(-57.5*(camera.angleY), 0,1, 0)
     drawHuman()
     glPopMatrix()
@@ -65,13 +70,15 @@ def keyPressed(*args):
     global camera
     fraction = 2
     if args[0] == b"a":
-        camera.angleY -= 0.05
-        camera.directionX = m.sin(camera.angleY)
-        camera.directionZ = -m.cos(camera.angleY)
+        pass
+        #camera.angleY -= 0.05
+        #camera.directionX = m.sin(camera.angleY)
+        #camera.directionZ = -m.cos(camera.angleY)
     elif args[0] == b"d":
-        camera.angleY += 0.05
-        camera.directionX = m.sin(camera.angleY)
-        camera.directionZ = -m.cos(camera.angleY)
+        pass
+        #camera.angleY += 0.05
+        #camera.directionX = m.sin(camera.angleY)
+        #camera.directionZ = -m.cos(camera.angleY)
     elif args[0] == b"w":
         camera.xPos += camera.directionX*fraction
         camera.zPos += camera.directionZ*fraction
@@ -80,22 +87,62 @@ def keyPressed(*args):
         camera.xPos -= camera.directionX * fraction
         camera.zPos -= camera.directionZ * fraction
         camera.yPos -= camera.directionY * fraction
+    elif args[0] == b" ":
+        camera.humanSpace = 0.1
     glutPostRedisplay()
+def HumanSpace():
+    global camera
+    if(camera.humanSpace != 0 and camera.humanSpace<2):
+        camera.humanSpace += 0.1
+    else:
+        camera.humanSpace = 0
 def mouse(button,state,x,y):
     global camera
-    camera.mouse_x = x
-    camera.mouse_y = y
     if GLUT_LEFT_BUTTON == 0:
         if GLUT_DOWN == 0:
             camera.mouse_left = 1
         if GLUT_UP == 0:
             camera.mouse_left= 0
 
+"""def mouseMotion(x,y):
+    global camera
+    if camera.mouse_left:
+        camera.directionXmouse += (x - camera.mouse_x)*0.0005
+        camera.directionYmouse += -(y - camera.mouse_y)*0.0005"""
 def mouseMotion(x,y):
     global camera
-    if camera.mouse_left :
-        camera.directionXmouse += (x - camera.mouse_x)*0.0005
-        camera.directionYmouse += -(y - camera.mouse_y)*0.0005
+    print(camera.mouse_x)
+    if (camera.mouse_x == 0):
+        camera.mouse_x = x
+        if (x > windowX / 2):
+            camera.angleY += 0.03
+            camera.directionX = m.sin(camera.angleY)
+            camera.directionZ = -m.cos(camera.angleY)
+        else:
+            camera.angleY -= 0.03
+            camera.directionX = m.sin(camera.angleY)
+            camera.directionZ = -m.cos(camera.angleY)
+    if(x>1900):
+        camera.mouse_x = x
+        camera.angleY += 0.03
+        camera.directionX = m.sin(camera.angleY)
+        camera.directionZ = -m.cos(camera.angleY)
+    if(x<20):
+        camera.mouse_x = x
+        camera.angleY -= 0.03
+        camera.directionX = m.sin(camera.angleY)
+        camera.directionZ = -m.cos(camera.angleY)
+    else:
+        if(x>camera.mouse_x):
+            camera.angleY += 0.03
+            camera.directionX = m.sin(camera.angleY)
+            camera.directionZ = -m.cos(camera.angleY)
+        else:
+            camera.angleY -= 0.03
+            camera.directionX = m.sin(camera.angleY)
+            camera.directionZ = -m.cos(camera.angleY)
+        camera.mouse_x = x
+
 def MouseWheel(*args):
     global camera
     if args[1] == -1:
@@ -109,7 +156,7 @@ def MouseWheel(*args):
 def main():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA)
-    glutInitWindowSize(1920, 1080)
+    glutInitWindowSize(windowX, windowY)
     glutInitWindowPosition(0, 0)
     glutIdleFunc(display)
     glutCreateWindow(b"Followw")
@@ -118,7 +165,7 @@ def main():
     glutKeyboardFunc(keyPressed)
     glutMouseWheelFunc(MouseWheel)
     glutMouseFunc(mouse)
-    glutMotionFunc(mouseMotion)
+    glutPassiveMotionFunc(mouseMotion)
     glutMainLoop()
     glEnable(GL_DEPTH_TEST)
 
